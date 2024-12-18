fe49b6b8a63186f43f8a8fe676abf29b28df8cf0
Amit Kumar Pandey <amitkumar.pandey052001@gmail.com>
2024-12-18 23:19:08 +0530

    Response Entity Class and Defining Base URL Using @RequestMapping

--- src/main/java/com/springboot/rest/api/controller/GetMappingController.java
+++ src/main/java/com/springboot/rest/api/controller/GetMappingController.java
@@ -1,20 +1,23 @@
 package com.springboot.rest.api.controller;
 
 import com.springboot.rest.api.beans.UserData;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 @RestController
+@RequestMapping("/user")//Used to define the base url
 public class GetMappingController {
     @GetMapping("/getUserDetails")
-    public UserData getUserDetails(){
+    public ResponseEntity<UserData> getUserDetails(){
         UserData userData =new UserData("Amit Kumar Pandey",00012,25);
         System.out.println("Getting UserData!....");
-        return userData;
+        //return new ResponseEntity<>(userData, HttpStatus.OK);
+        //return ResponseEntity.ok(userData);
+        return ResponseEntity.ok().header("Status","Done").body(userData);
     }
 
     @GetMapping("/listOfUserDetails")
